d0230211f8ccc915adc88c69f77cc0a42e19e9ec
fernandobn <154335390+fernandobn@users.noreply.github.com>
2025-02-10 18:07:55 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,8 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',  // ✅ Permite que Render acceda a la aplicación
     port: process.env.PORT || 4173,  // ✅ Usa el puerto asignado por Render o el predeterminado (4173)
+  },
+  preview: {
+    allowedHosts: ["sistemaresidencialfrontend.onrender.com"], // ✅ Permite la vista previa en Render
   }
 })
